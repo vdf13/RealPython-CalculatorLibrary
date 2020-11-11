4ee55c6d89a2fd9e8ae8e32634ede4fbef234050
vdf13 <vdf13@free.fr>
2020-11-11 11:46:43 +0100

    Add test_calculator

---
+++ test_calculator.py
@@ -0,0 +1,13 @@
+"""
+Unit tests for the calculator library
+"""
+
+import calculator
+
+
+class TestCalculator:
+    def test_addition(self):
+        assert 5 == calculator.add(3, 2)
+
+    def test_subtraction(self):
+        assert 2 == calculator.subtract(4, 2)
