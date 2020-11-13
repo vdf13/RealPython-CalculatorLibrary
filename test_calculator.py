68d85950f6d47edb4e65511ccdc1fb77b52bf289
vdf13 <vdf13@free.fr>
2020-11-13 19:24:29 +0100

    Add multiplication test without the code in main program

--- test_calculator.py
+++ test_calculator.py
@@ -11,3 +11,6 @@ class TestCalculator:
 
     def test_subtraction(self):
         assert 2 == calculator.subtract(4, 2)
+
+    def test_multiplication(self):
+        assert 69 == calculator.multiply(3, 23) 
