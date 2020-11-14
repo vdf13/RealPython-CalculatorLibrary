1c87e763626cf77cc045af378e5ecd30fcd1df1a
vdf13 <vdf13@free.fr>
2020-11-14 12:01:59 +0100

    Add mutliplication test , add a missing space

--- test_calculator.py
+++ test_calculator.py
@@ -13,4 +13,5 @@ class TestCalculator:
         assert 2 == calculator.subtract(4, 2)
 
     def test_multiplication(self):
-        assert 69 == calculator.multiply(3, 23) 
+        assert 69 == calculator.multiply(3, 23)
+
